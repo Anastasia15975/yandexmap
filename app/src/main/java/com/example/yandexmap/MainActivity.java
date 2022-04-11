0de70d8d8f65ff0dc947b59fd7a93b52d9331c44
LisovaAnastasia <anonim15975@gmail.com>
2022-04-11 08:33:52 +0300

    init commit

---
+++ app/src/main/java/com/example/yandexmap/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.yandexmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mapview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("8a5ca947-a710-43d7-8a4c-f3a47a625e1d");
+        MapKitFactory.initialize(this);
+
+        // Укажите имя activity вместо map.
+        setContentView(R.layout.activity_main);
+        mapview = findViewById(R.id.mapview);
+        mapview.getMap().move(
+                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapview.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapview.onStart();
+        MapKitFactory.getInstance().onStart();
+    }
+}
